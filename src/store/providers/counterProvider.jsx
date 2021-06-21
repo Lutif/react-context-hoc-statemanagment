b5f05c45950a13d79c5bfea0105924ec913dd0fa
lutif <lutif.mandhro@gmail.com>
2021-06-21 10:33:08 +0500

    useReducer where needed

--- src/store/providers/counterProvider.jsx
+++ src/store/providers/counterProvider.jsx
@@ -1,14 +1,12 @@
-import React, { useReducer } from "react";
+import React, { useState } from "react";
 import { CounterContext } from "../context";
 
 export const CounterProvider = (props) => {
-  const [counter, setCounter] = useReducer((state, payload) => {
-    return payload ? state + payload : null;
-  }, 0);
+  const [counter, setCounter] = useState(0);
 
   const value = {
     counter,
-    setCounter
+    setCounter: (value) => setCounter((current) => current + value)
   };
 
   return (
--- src/store/providers/themeProvider.jsx
+++ src/store/providers/themeProvider.jsx
@@ -1,11 +1,37 @@
 import React, { useReducer } from "react";
 import { ThemeContext } from "../context";
+
+const intialTheme = {
+  color: "Red",
+  fontSize: 22
+};
+const themeReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "color":
+      return {
+        ...state,
+        color: payload
+      };
+    case "fontSize":
+      return {
+        ...state,
+        fontSize: payload
+      };
+    default:
+      return {
+        ...state
+      };
+  }
+};
+
 export const ThemeProvider = ({ children }) => {
-  const [color, setColor] = useReducer((state, payload) => payload, "Red");
+  const [theme, setTheme] = useReducer(themeReducer, intialTheme); // can consist of complex state for example, hover, secondary, primary, ... colors etc
   const value = {
-    color,
-    setColor
+    color: theme?.color,
+    setColor: (value) => setTheme({ type: "color", payload: value })
   };
+
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
   );
